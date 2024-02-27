db15809c1276428b88ba1ce8115a3509345b7a31
antonimalinowski <antonimalinowski.kontakt@gmail.com>
2024-02-27 11:53:42 +0100

    Add project files.

---
+++ steganography/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace steganography;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ steganography/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace steganography;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
